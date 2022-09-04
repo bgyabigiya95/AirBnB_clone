34677f8eb6c061b9b52b7075aa2a8ad315a83656
Abigiya Haile <bgyabigiya95@gmail.com>
2022-09-04 08:09:55 -0700

    commit

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,33 @@
+ts for Amenity model
+"""
+
+
+import unittest
+import datetime
+
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """Testing Amenity Module"""
+
+    def setUp(self):
+        """An instance just for tests"""
+        self.a1 = Amenity()
+        self.a2 = Amenity()
+
+    def test_instances(self):
+        """Test the instances"""
+        self.assertTrue(isinstance(self.a1, Amenity))
+        self.assertTrue(isinstance(self.a2, Amenity))
+        self.assertTrue(hasattr(self.a1, "name"))
+
+    def test_name(self):
+        """test the name"""
+        self.assertEqual(type(self.a1.name), str)
+        self.assertEqual(self.a1.name, "")
+        self.assertNotEqual(self.a1.name, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_models/test_console.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Tests for console
+"""
+
+
+import unittest
+import datetime
+
+from console import HBNBCommand
